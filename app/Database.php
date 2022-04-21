d3e99305295bfc7ab1971274f8b688a177f947ba
ssydney77 <103968309+ssydney77@users.noreply.github.com>
2022-04-21 12:39:25 +0200

    Add files via upload

---
+++ app/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+use PDO;
+
+class Database{
+
+    private $dsn="mysql:dbname=gsb;host=localhost";
+    private $login="root";
+    private $password="root";
+    private $pdo;
+
+    public function __construct(){
+            
+        $this->pdo = new PDO($this->dsn,$this->login,$this->password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+    }
+
+    public function query($query,$params=[]){
+        if($params){
+            $req=$this->pdo->prepare($query);
+            $req->execute($params);
+        }
+        else{
+            $req=$this->pdo->query($query);
+        }   
+        return $req;
+    }
+}
